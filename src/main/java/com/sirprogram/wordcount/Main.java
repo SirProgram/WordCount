cf8b33cd14ed84ff78f1930524e873c788ebab22
Jonathan Scherrer <jonathan.scherrer@gmail.com>
2022-11-08 21:51:25 +0100

    Tidy up autogeneration and add test structure

--- src/main/java/com/sirprogram/wordcount/Main.java
+++ src/main/java/com/sirprogram/wordcount/Main.java
@@ -2,5 +2,8 @@ package com.sirprogram.wordcount;
 
 public class Main {
 
+    public static void main(String[] args) {
 
+        System.out.println("Program Started");
+    }
 }
---
+++ src/main/test/com/sirprogram/wordcount/MainTest.java
@@ -0,0 +1,21 @@
+package com.sirprogram.wordcount;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+        Main.main(new String[]{});
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testCanRunProgram() {
+    }
+}
