40175ec4de876bf00a9b0f349c81d2c37853d030
Jonathan Scherrer <jonathan.scherrer@gmail.com>
2022-11-12 21:40:28 +0100

    Use Files.readAllLines in WordCountFileReader, tidy imports.

--- src/main/java/com/sirprogram/wordcount/WordCountFileReader.java
+++ src/main/java/com/sirprogram/wordcount/WordCountFileReader.java
@@ -3,10 +3,8 @@ package com.sirprogram.wordcount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.util.ArrayList;
+import java.nio.file.Files;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,13 +14,10 @@ public class WordCountFileReader {
 
     public List<String> readFile(String path) {
         File inputFile = new File(path);
-        List<String> readLines = new ArrayList<>();
+        List<String> readLines;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
-            String line;
-            for (line = reader.readLine(); line != null; line = reader.readLine()) {
-                readLines.add(line);
-            }
+        try {
+            readLines = Files.readAllLines(inputFile.toPath());
         } catch (Exception e) {
             LOG.error("Failed to read file: " + path, e);
             return Collections.emptyList();
--- src/main/java/com/sirprogram/wordcount/WordCounter.java
+++ src/main/java/com/sirprogram/wordcount/WordCounter.java
@@ -1,6 +1,9 @@
 package com.sirprogram.wordcount;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
